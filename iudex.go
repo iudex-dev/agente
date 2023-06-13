8ce8f8bdc941890d1ead80faa32d4103d2637378
Pau Fernandez <pau.fernandez@upc.edu>
2023-06-13 17:03:54 +0200

    Parse carcer output

--- iudex.go
+++ iudex.go
@@ -15,20 +15,33 @@ func exitWithError(message string) {
 	os.Exit(1)
 }
 
-func main() {
-	out, err := Compile("program.cc", "program")
+type CarcerResult struct {
+	CpuTime  int64  `json:"cpu_time"`
+	RealTime int64  `json:"real_time"`
+	Memory   int64  `json:"memory"`
+	ExitCode int    `json:"exit_code"`
+	Report   string `json:"report"`
+}
+
+func processSubmission(filename string) CarcerResult {
+	out, err := Compile(filename, "iudex-binary")
 	if err != nil {
-		exitWithError(out)	
+		exitWithError(out)
 	}
 
-	out, err = ExecSandboxed("program")
+	out, err = ExecSandboxed("iudex-binary")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Execution error: %s\n", err)
 	}
 
 	reader := strings.NewReader(out)
 	dec := json.NewDecoder(reader)
-	result := map[string]interface{}{}
+	result := CarcerResult{}
 	dec.Decode(&result)
+	return result
+}
+
+func main() {
+	result := processSubmission("program.cc")
 	fmt.Printf("%+v", result)
 }
