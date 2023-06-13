11630f0aa517503a75dcd65efc456fda8a4c09a1
Pau Fernandez <pau.fernandez@upc.edu>
2023-06-13 15:50:44 +0200

    Mejoras

--- iudex.go
+++ iudex.go
@@ -1,20 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 )
+
+func exitWithError(message string) {
+	fmt.Fprintf(os.Stderr, `{
+  "status": "Compilation error",
+  "output": "%s"
+}`, message)
+	os.Exit(1)
+}
 
 func main() {
 	out, err := Compile("program.cc", "program")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Compilation error: %s", err)
+		exitWithError(out)	
 	}
-	fmt.Printf("OUTPUT:\n%s", out)
 
 	out, err = ExecSandboxed("program")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Execution error: %s\n", err)
 	}
-	fmt.Printf("OUTPUT:\n%s", out)
+
+	reader := strings.NewReader(out)
+	dec := json.NewDecoder(reader)
+	result := map[string]interface{}{}
+	dec.Decode(&result)
+	fmt.Printf("%+v", result)
 }
--- ops.go
+++ ops.go
@@ -3,20 +3,39 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"strings"
 )
 
-func Compile(sourcePath, binaryName string) (output string, err error) {
-	print("Compiling... ")
-	defer println("ok")
+func checkDockerImage(imageName string) bool {
+	// Check that the docker image exists
+	outBytes, err := exec.Command(
+		"docker", "image", "list", "-a",
+	).CombinedOutput()
+	if err != nil {
+		return false
+	}
+	lines := strings.Split(string(outBytes), "\n")
+	for _, line := range lines {
+		if pos := strings.Index(line, " "); pos != -1 {
+			if strings.Compare(line[:pos], imageName) == 0 {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func Compile(sourceName, binaryName string) (output string, err error) {
+	if ok := checkDockerImage("iudex-compiler-clang"); !ok {
+		return "", fmt.Errorf("docker image not found for compiler")
+	}
 
 	outBytes, err := exec.Command(
-		"docker", "run",
-		"--rm",
-		"-v", ".:/iudex",
-		"iudex-compile-clang",
-		"clang++", sourcePath, "-o", 
-		fmt.Sprintf("/iudex/%s", binaryName), 
-		"-static",
+		"docker", "run", "--rm", "-v", ".:/iudex",
+		"iudex-compiler-clang",
+		"clang++", "-static",	"-o",
+		fmt.Sprintf("/iudex/%s", binaryName),
+		fmt.Sprintf("/iudex/%s", sourceName),
 	).CombinedOutput()
 
 	output = string(outBytes)
@@ -24,15 +43,12 @@ func Compile(sourcePath, binaryName string) (output string, err error) {
 }
 
 func ExecSandboxed(binaryName string) (output string, err error) {
-	print("Running... ")
-	defer println("ok")
-
 	outBytes, err := exec.Command(
 		"docker", "run",
 		"--rm",
 		"-v", ".:/iudex",
 		"iudex-carcer",
-		"carcer", fmt.Sprintf("/iudex/%s", binaryName),
+		"carcer", "-o", "output", "-e", "error", fmt.Sprintf("/iudex/%s", binaryName),
 	).CombinedOutput()
 
 	output = string(outBytes)
